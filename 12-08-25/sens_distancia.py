4afd4f7d4cc3f73930cf53d7505997b71262a6b4
fllredhammer423-jpg <fll.redhammer423@gmail.com>
2025-12-12 16:08:56 -0300

    Delete 12-08-25/sens_distancia.py

--- 12-08-25/sens_distancia.py
+++
@@ -1,9 +0,0 @@
-
-from pybricks.hubs import PrimeHub
-from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor
-from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
-from pybricks.robotics import DriveBase
-from pybricks.tools import wait, StopWatch
-
-hub = PrimeHub()
-
